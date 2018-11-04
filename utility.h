90ac6fed5d75139f6a5c3edf25b1037f6faeda05
Dariusz Rajtak (DRA) <DRA@kmd.dk>
2018-11-04 16:18:15 +0100

    initial, sending works

---
+++ utility.h
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. 
+
+#ifndef UTILITY_H
+#define UTILITY_H
+
+void parseTwinMessage(DEVICE_TWIN_UPDATE_STATE, const char *);
+bool readMessage(int, char *);
+
+void SensorInit(void);
+
+void blinkLED(void);
+void blinkSendConfirmation(void);
+int getInterval(void);
+
+float readPressure();
+float readTemperature();
+float readHumidity();
+
+#endif /* UTILITY_H */
